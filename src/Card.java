d748dc9ffe9bb0e50a36ca215c07602ab2e55769
Bhavya Govil <bhavya0836govil@gmail.com>
2025-01-09 14:30:54 +0000

    adding deck and card

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,26 @@
 public class Card {
+    private final Suit Suit;
+    private final Rank Rank;
+    private final int Value;
+    private boolean isAce;
 
+    //constructor:
+    public Card(Suit suit, Rank rank, int value){
+        this.Suit = suit;
+        this.Rank = rank;
+        this.Value = value;
+    }
 
+    public Rank getRank() {
+        return Rank;
+    }
+
+    public Suit getSuit() {
+        return Suit;
+    }
+
+    public int getValue(){
+        return Value;
+    }
 
 }
